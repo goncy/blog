46be562e0d91c5898e687f76a4beee64b18aeedd
goncy <gonzalo.pozzo4@gmail.com>
2020-03-23 18:01:45 -0300

    Initial commit

---
+++ src/website-config.ts
@@ -0,0 +1,68 @@
+export interface WebsiteConfig {
+  title: string;
+  description: string;
+  coverImage: string;
+  logo: string;
+  /**
+   * Specifying a valid BCP 47 language helps screen readers announce text properly.
+   * See: https://dequeuniversity.com/rules/axe/2.2/valid-lang
+   */
+  lang: string;
+  /**
+   * blog full path, no ending slash!
+   */
+  siteUrl: string;
+  /**
+   * full url, no username
+   */
+  facebook?: string;
+  /**
+   * full url, no username
+   */
+  twitter?: string;
+  /**
+   * full url, no username
+   */
+  github?: string;
+  /**
+   * hide or show all email subscribe boxes
+   */
+  showSubscribe: boolean;
+  /**
+   * create a list on mailchimp and then create an embeddable signup form. this is the form action
+   */
+  mailchimpAction?: string;
+  /**
+   * this is the hidden input field name
+   */
+  mailchimpName?: string;
+  /**
+   * name and id of the mailchimp email field
+   */
+  mailchimpEmailFieldName?: string;
+  /**
+  /**
+   * Meta tag for Google Webmaster Tools
+   */
+  googleSiteVerification?: string;
+  /**
+  /**
+   * Appears alongside the footer, after the credits
+   */
+  footer?: string;
+}
+
+const config: WebsiteConfig = {
+  title: 'Gonzalo Pozzo',
+  description: 'Personal thoughts',
+  coverImage: 'img/blog-cover.jpg',
+  logo: 'img/goncy-logo.png',
+  lang: 'en',
+  siteUrl: 'http://gonzalopozzo.com',
+  facebook: 'https://www.facebook.com/goncy.pozzo',
+  twitter: 'https://twitter.com/goncy',
+  github: 'https://github.com/goncy',
+  showSubscribe: false,
+};
+
+export default config;
