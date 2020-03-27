dd8fcfdc7206c56edf95d5aaa8bd98e7e35d884b
goncy <gonzalo.pozzo4@gmail.com>
2020-03-27 14:02:12 -0300

    Add google analyics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,40 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   siteMetadata: {
-    title: 'Gonzalo Pozzo - Frontend Developer',
-    description: 'Personal thoughts',
-    siteUrl: 'http://gonzalopozzo.com',
+    title: "Gonzalo Pozzo - Frontend Developer",
+    description: "Personal thoughts",
+    siteUrl: "http://gonzalopozzo.com",
   },
   mapping: {
-    'MarkdownRemark.frontmatter.author': 'AuthorYaml',
+    "MarkdownRemark.frontmatter.author": "AuthorYaml",
   },
   plugins: [
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'content',
-        path: path.join(__dirname, 'src', 'content'),
+        name: "content",
+        path: path.join(__dirname, "src", "content"),
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: 'margin-bottom: 1rem',
+              wrapperStyle: "margin-bottom: 1rem",
             },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          'gatsby-remark-abbr',
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
+          "gatsby-remark-abbr",
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 1170,
               quality: 90,
@@ -43,29 +43,29 @@ module.exports = {
         ],
       },
     },
-    'gatsby-transformer-json',
+    "gatsby-transformer-json",
     {
-      resolve: 'gatsby-plugin-canonical-urls',
+      resolve: "gatsby-plugin-canonical-urls",
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: "https://gatsby-casper.netlify.com",
       },
     },
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-typescript',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-yaml',
-    'gatsby-plugin-feed',
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-typescript",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-yaml",
+    "gatsby-plugin-feed",
     {
-      resolve: 'gatsby-plugin-postcss',
+      resolve: "gatsby-plugin-postcss",
       options: {
-        postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
+        postCssPlugins: [require("postcss-color-function"), require("cssnano")()],
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: "UA-62524880-11",
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
@@ -73,7 +73,7 @@ module.exports = {
         // Disable analytics for users with `Do Not Track` enabled
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
+        exclude: ["/preview/**"],
         // Specifies what percentage of users should be tracked
         sampleRate: 100,
         // Determines how often site speed tracking beacons will be sent
